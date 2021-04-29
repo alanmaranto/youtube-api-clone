3a8ece8def6d630cf9a821e6a4dcb1c94d11fc9c
alan <alanarostegui@hotmail.com>
2021-04-29 14:50:10 -0500

    change security config

---
+++ db/config/db.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    define: { charset: "utf8", collate: "utf8_general_ci", timestamps: true },
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
